0a21872dd71ca290509c89545b25b20f495e4d50
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-05-25 22:57:48 +0300

    Refactor project

---
+++ otp/builldGraph.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java.exe -Xmx5G -jar otp-1.4.0-shaded.jar --build graphs/current
---
+++ otp/startServer.sh
@@ -0,0 +1,1 @@
+java.exe -Xmx5G -jar otp-1.4.0-shaded.jar --router current --graphs graphs --server
